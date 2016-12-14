8b83dd774db7c9a0ca11a4fa8697598d944c8788
jakobsbode <jakobsbode@mi.fu-berlin.de>
2016-12-14 16:58:11 +0100

    began init sequence

---
+++ enzevalos_iphone/InitViewController.swift
@@ -0,0 +1,15 @@
+//
+//  InitViewController.swift
+//  enzevalos_iphone
+//
+//  Created by jakobsbode on 14.12.16.
+//  Copyright © 2016 fu-berlin. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class InitViewController : UIViewController {
+    
+
+}
