58be588a72873892702db009886de2c52900dd6a
jakobsbode <jakobsbode@mi.fu-berlin.de>
2017-01-04 13:39:00 +0100

    removed unnecessary comments

--- enzevalos_iphone/MailObject.swift
+++ enzevalos_iphone/MailObject.swift
@@ -154,8 +154,7 @@ class Mail: Comparable {
                     
                     let decTrash = (try? CryptoHandler.getHandler().pgp.decryptData(body!.dataUsingEncoding(NSUTF8StringEncoding)!, passphrase: nil, verifyWithPublicKey: signatureKey, signed: &signed, valid: &valid, integrityProtected: &integrityProtected))
                     if decTrash != nil {
-                        self.decryptedBody = String(data: decTrash!, encoding: NSUTF8StringEncoding)//String(data: (try? CryptoHandler.getHandler().pgp.decryptData(body!.dataUsingEncoding(NSUTF8StringEncoding)!, passphrase: nil, verifyWithPublicKey: signatureKey, signed: &signed, valid: &valid, integrityProtected: &integrityProtected))! as NSData, encoding: NSUTF8StringEncoding)
-                        //print(String(data: (try? CryptoHandler.getHandler().pgp.decryptData(body.dataUsingEncoding(NSUTF8StringEncoding)!, passphrase: nil, verifyWithPublicKey: nil, signed: &signed, valid: &valid, integrityProtected: &integrityProtected), encoding: NSUTF8StringEncoding)))
+                        self.decryptedBody = String(data: decTrash!, encoding: NSUTF8StringEncoding)
                         self.isVerified = Bool(valid)
                         
                         print(" signed: ",signed," valid: ",valid, " integrityProtected: ",integrityProtected)
@@ -165,21 +164,6 @@ class Mail: Comparable {
                         }
                     }
                     
-                    /*if (try? CryptoHandler.getHandler().pgp.decryptData(body!.dataUsingEncoding(NSUTF8StringEncoding)!, passphrase: nil, verifyWithPublicKey: nil, signed: &signed, valid: &valid, integrityProtected: &integrityProtected) as NSData?) != nil && ((try? CryptoHandler.getHandler().pgp.decryptData(body!.dataUsingEncoding(NSUTF8StringEncoding)!, passphrase: nil, verifyWithPublicKey: nil, signed: &signed, valid: &valid, integrityProtected: &integrityProtected))! as NSData?) != nil{
-                    
-                        self.decryptedBody = String(data: (try? CryptoHandler.getHandler().pgp.decryptData(body!.dataUsingEncoding(NSUTF8StringEncoding)!, passphrase: nil, verifyWithPublicKey: signatureKey, signed: &signed, valid: &valid, integrityProtected: &integrityProtected))! as NSData, encoding: NSUTF8StringEncoding)
-                    //print(String(data: (try? CryptoHandler.getHandler().pgp.decryptData(body.dataUsingEncoding(NSUTF8StringEncoding)!, passphrase: nil, verifyWithPublicKey: nil, signed: &signed, valid: &valid, integrityProtected: &integrityProtected), encoding: NSUTF8StringEncoding)))
-                        self.isVerified = Bool(valid)
-                        
-                        print(" signed: ",signed," valid: ",valid, " integrityProtected: ",integrityProtected)
-                        
-                        if signatureKey != nil && !valid && signed {
-                            self.trouble = true
-                        }
-                    }*/
-                //print(try? CryptoHandler.getHandler().pgp.decryptData(body.dataUsingEncoding(NSUTF8StringEncoding)!, passphrase: nil, verifyWithPublicKey: nil, signed: &signed, valid: &valid, integrityProtected: &integrityProtected))
-                //let content = try? CryptoHandler.getHandler().pgp.decryptData(body.dataUsingEncoding(NSUTF8StringEncoding)!, passphrase: nil)
-                //print(content)
                 } catch _ {
                 
                     self.trouble = true
