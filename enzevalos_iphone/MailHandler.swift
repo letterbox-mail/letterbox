df11e74c7cd58a40f1af60e585603bc52c390396
Joscha <joscha@virtual-artz.de>
2017-02-09 16:18:55 +0100

    Fix crashing bug when no subject is set

--- enzevalos_iphone/MailHandler.swift
+++ enzevalos_iphone/MailHandler.swift
@@ -315,7 +315,7 @@ class MailHandler {
                         //TODO: Fix UID -> UInt64, Int64, UInt 32...??????
                         // TODO: Fix decryption
                         
-                        let mail = DataHandler.handler.createMail(UInt64(message.uid), sender: header.from, receivers: rec, cc: cc, time: header.date, received: true, subject: header.subject, body: body, flags: message.flags)
+                        let mail = DataHandler.handler.createMail(UInt64(message.uid), sender: header.from, receivers: rec, cc: cc, time: header.date, received: true, subject: header.subject ?? "", body: body, flags: message.flags)
                       //  mail.decryptIfPossible()
                         /*Jakob prototypeänderung Ende*/
                         self.delegate?.addNewMail(mail)
