b31545ab34671172071f55c509e4ad20b8eb9870
jakobsbode <jakobsbode@mi.fu-berlin.de>
2017-02-15 12:43:11 +0100

    commit for pull

---
+++ enzevalos_iphone/PGPTemporaryDecryptionObject.swift
@@ -0,0 +1,9 @@
+//
+//  PGPTemporaryDecryptionObject.swift
+//  enzevalos_iphone
+//
+//  Created by jakobsbode on 09.02.17.
+//  Copyright © 2017 fu-berlin. All rights reserved.
+//
+
+import Foundation
