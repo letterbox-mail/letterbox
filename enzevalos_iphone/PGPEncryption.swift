f932d83c0e0add51e9e9932dbfa1fad584a467fa
Oliver Wiese <oliver.wiese@fu-berlin.de>
2017-03-03 11:59:56 +0100

    signed from let to var.

--- enzevalos_iphone/PGPEncryption.swift
+++ enzevalos_iphone/PGPEncryption.swift
@@ -128,9 +128,9 @@ class PGPEncryption : Encryption {
             var data: NSData?
             var temp = keyManager.pgp.decryptDataFirstPart(bodyData, passphrase: nil, integrityProtected: nil, error: nil)
             var maybeUsedKeys: [String] = []
-            let signed = UnsafeMutablePointer<ObjCBool>.alloc(1)
+            var signed = UnsafeMutablePointer<ObjCBool>.alloc(1)
             signed[0] = false
-            let valid = UnsafeMutablePointer<ObjCBool>.alloc(1)
+            var valid = UnsafeMutablePointer<ObjCBool>.alloc(1)
             valid[0] = false
             do {
                 data = temp.plaintextData
@@ -153,6 +153,7 @@ class PGPEncryption : Encryption {
                         if let key = self.keyManager.getKey(maybeUsedKey) {
                             let done : ObjCBool = (self.keyManager.pgp.decryptDataSecondPart(temp, verifyWithPublicKey: key.key, signed: signed, valid: valid, error: nil)[0])
                             if done {
+                                print("Set signed to: \(signed.memory.boolValue)")
                                 mail.isSigned = signed.memory.boolValue
                                 mail.isCorrectlySigned = valid.memory.boolValue
                             }
