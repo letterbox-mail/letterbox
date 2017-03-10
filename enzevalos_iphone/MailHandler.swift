3b095ad801e84b3f530f67c054a4adb8cdb465d7
Joscha <joscha@virtual-artz.de>
2017-03-10 15:20:16 +0100

    Fix bug in listView

--- enzevalos_iphone/MailHandler.swift
+++ enzevalos_iphone/MailHandler.swift
@@ -339,6 +339,8 @@ class MailHandler {
     }
 
     func loadMoreMails(record: KeyRecord, newMailCallback: (() -> ()), completionCallback: ((error: Bool) -> ())) {
-        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(2 * Double(NSEC_PER_SEC))), dispatch_get_main_queue(), { completionCallback(error: true) })
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(2 * Double(NSEC_PER_SEC))), dispatch_get_main_queue(), {
+            completionCallback(error: false)
+        })
     }
 }
