b9536653f5aa7fabfbe08bbf9192075be1705106
Oliver Wiese <oliver.wiese@fu-berlin.de>
2017-06-01 15:51:32 +0200

    fix #82

--- enzevalos_iphone/AppDelegate.swift
+++ enzevalos_iphone/AppDelegate.swift
@@ -115,6 +115,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+        resetApp()
+        
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
@@ -130,6 +132,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        resetApp()
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
