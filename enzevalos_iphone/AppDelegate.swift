147844cfb538d1d087de00f28e4942e15f31f8cb
Joscha <joscha@virtual-artz.de>
2017-12-21 14:54:13 +0100

    Removed settings from the settings app

--- enzevalos_iphone/AppDelegate.swift
+++ enzevalos_iphone/AppDelegate.swift
@@ -82,6 +82,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         }
     }
 
+    // Option removed from Settings app, but this might still be usefull in the future
     func resetApp() {
         if UserDefaults.standard.bool(forKey: "reset") {
             DataHandler.handler.reset()
