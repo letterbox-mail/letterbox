bbed5c237a456584821561236aeb7ad42e2bee63
kondeichmann <k.deichmann@mac.com>
2018-01-08 13:12:34 +0100

    add basic structure for internal Invitation classes + extension for SendViewController

--- enzevalos_iphone/Invitation/InvitationHelper.swift
+++ enzevalos_iphone/Invitation/InvitationHelper.swift
@@ -7,3 +7,35 @@
 //
 
 import UIKit
+
+extension String {
+
+	func words(inRange range: NSRange) -> [(word: String, index: Int)] {
+
+		let strings = self.components(separatedBy: .whitespacesAndNewlines)
+		var result = [(word: String, index: Int)]()
+
+		var index = 0
+		let start = range.location
+		let length = range.length
+
+		for string in strings {
+
+			guard (index < start + length) else {
+				break
+			}
+
+			guard (index + string.count >= start) else {
+				index += (1 + string.count)
+				continue
+			}
+
+			result.append((word: string, index: index))
+			index += (1 + string.count)
+		}
+
+		return result.filter({ (word: String, index: Int) -> Bool in
+			return (word.isEmpty == false)
+		})
+	}
+}
