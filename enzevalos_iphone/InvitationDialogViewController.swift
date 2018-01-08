3045f970b55570d2a2e8208666d7e05f95496a3e
kondeichmann <k.deichmann@mac.com>
2018-01-08 10:53:34 +0100

    Invitation Class structure

---
+++ enzevalos_iphone/InvitationDialogViewController.swift
@@ -0,0 +1,9 @@
+//
+//  InvitationDialogViewController.swift
+//  enzevalos_iphone
+//
+//  Created by Konstantin Deichmann on 08.01.18.
+//  Copyright © 2018 fu-berlin. All rights reserved.
+//
+
+import UIKit
