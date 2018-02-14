796bde3ebe03f808ce5c88926c710449931d40ec
Oliver Wiese <oliver.wiese@fu-berlin.de>
2018-02-14 16:23:10 +0100

    update core data

---
+++ enzevalos_iphone/Record+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Record+CoreDataClass.swift
+//  enzevalos_iphone
+//
+//  Created by Oliver Wiese on 14.02.18.
+//  Copyright © 2018 fu-berlin. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Record)
+public class Record: NSManagedObject {
+
+}
