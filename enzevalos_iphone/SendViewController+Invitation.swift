f20d4a7fd6671dd389daa7de9362feff1c9b6a75
Joscha <joscha@virtual-artz.de>
2018-02-22 17:11:40 +0100

    fixes a bug where encryption would be offered in the subject field

--- enzevalos_iphone/SendViewController+Invitation.swift
+++ enzevalos_iphone/SendViewController+Invitation.swift
@@ -12,280 +12,284 @@ import UIKit
 
 struct InvitationSelection {
 
-	var selectedWords 	= Set<NSRange>()
-	var code			: String?
-	var didShowDialog	= false
+    var selectedWords = Set<NSRange>()
+    var code: String?
+    var didShowDialog = false
 }
 
 // MARK: - SendViewController Extension
 
 extension SendViewController {
 
-	func htmlMessage() -> String? {
+    func htmlMessage() -> String? {
 
-		guard
-			let resource = Bundle.main.url(forResource: "invitationText", withExtension: "html"),
-			let data = try? Data(contentsOf: resource),
-			let htmlString = String(data: data, encoding: .utf8), (self.isEligibleForInvitation() == true && self.invitationSelection.selectedWords.isEmpty == false) else {
-				return nil
-		}
+        guard
+            let resource = Bundle.main.url(forResource: "invitationText", withExtension: "html"),
+            let data = try? Data(contentsOf: resource),
+            let htmlString = String(data: data, encoding: .utf8), (self.isEligibleForInvitation() == true && self.invitationSelection.selectedWords.isEmpty == false) else {
+                return nil
+        }
 
-		var text: String = self.textView.text
-		let textsToEncrypt = self.invitationSelection.selectedWords.sorted { (lhs, rhs) -> Bool in
-			return lhs.location < rhs.location
-		}.map { (range) -> String in
-			return (text as NSString).substring(with: range)
-		}
+        var text: String = self.textView.text
+        let textsToEncrypt = self.invitationSelection.selectedWords.sorted { (lhs, rhs) -> Bool in
+            return lhs.location < rhs.location
+        }.map { (range) -> String in
+            return (text as NSString).substring(with: range)
+        }
 
-		let cipherText = SwiftPGP().symmetricEncrypt(textToEncrypt: [textsToEncrypt.joined(separator: "\n")], armored: true)
-		let texts = textsToEncrypt.map { _ -> String in
-			return String.random(length: 10)
-		}
+        let cipherText = SwiftPGP().symmetricEncrypt(textToEncrypt: [textsToEncrypt.joined(separator: "\n")], armored: true)
+        let texts = textsToEncrypt.map { _ -> String in
+            return String.random(length: 10)
+        }
 
-		guard
-			let urlTexts = texts.joined(separator: ",").urlString,
-			let cipher = cipherText.chiphers.first?.urlString else {
-				return nil
-		}
+        guard
+            let urlTexts = texts.joined(separator: ",").urlString,
+            let cipher = cipherText.chiphers.first?.urlString else {
+                return nil
+        }
 
-		let link = "http://enzevalos.konstantindeichmann.de?text=\(urlTexts)&cipher=\(cipher)"
+        let link = "http://enzevalos.konstantindeichmann.de?text=\(urlTexts)&cipher=\(cipher)"
 
-		let locations = self.invitationSelection.selectedWords.sorted { (lhs, rhs) -> Bool in
-			return rhs.location < lhs.location
-		}
+        let locations = self.invitationSelection.selectedWords.sorted { (lhs, rhs) -> Bool in
+            return rhs.location < lhs.location
+        }
 
-		for (index, range) in locations.enumerated() {
-			text = (text as NSString).replacingCharacters(in: range, with: "<a class=\"encrypted-text\">\(texts[index])</a>")
-		}
+        for (index, range) in locations.enumerated() {
+            text = (text as NSString).replacingCharacters(in: range, with: "<a class=\"encrypted-text\">\(texts[index])</a>")
+        }
 
-		if (self.invitationSelection.code == nil) {
-			self.invitationSelection.code = cipherText.password
-		}
+        if (self.invitationSelection.code == nil) {
+            self.invitationSelection.code = cipherText.password
+        }
 
-		return String(format: htmlString, text, link, link)
-	}
+        return String(format: htmlString, text, link, link)
+    }
 
-	fileprivate func removeAllInvitationMarks() {
-		if (self.invitationSelection.selectedWords.isEmpty == false) {
-			self.invitationSelection.selectedWords = Set<NSRange>()
-			self.layoutText()
-		}
-	}
+    fileprivate func removeAllInvitationMarks() {
+        if (self.invitationSelection.selectedWords.isEmpty == false) {
+            self.invitationSelection.selectedWords = Set<NSRange>()
+            self.layoutText()
+        }
+    }
 
-	fileprivate func menuControllerItems(for textView: UITextView) -> [UIMenuItem]? {
+    fileprivate func menuControllerItems(for textView: UITextView) -> [UIMenuItem]? {
 
-		if (self.textView.selectedRange.location == 0 && self.textView.selectedRange.length == 0) {
-			return nil
-		}
+        if (self.textView.selectedRange.location == 0 && self.textView.selectedRange.length == 0) {
+            return nil
+        }
 
-		let selectedRange = self.invitationSelection.selectedWords.first { (range) -> Bool in
-			return range.isInRange(of: self.textView.selectedRange)
-		}
+        let selectedRange = self.invitationSelection.selectedWords.first { (range) -> Bool in
+            return range.isInRange(of: self.textView.selectedRange)
+        }
 
-		guard selectedRange != nil else {
-			return [UIMenuItem(title: NSLocalizedString("Invitation.Encrypt", comment: ""), action: #selector(self.markSelectedText))]
-		}
+        guard selectedRange != nil else {
+            return [UIMenuItem(title: NSLocalizedString("Invitation.Encrypt", comment: ""), action: #selector(self.markSelectedText))]
+        }
 
-		return [UIMenuItem(title: NSLocalizedString("Invitation.Decrypt", comment: ""), action: #selector(self.unmarkSelectedText))]
-	}
+        return [UIMenuItem(title: NSLocalizedString("Invitation.Decrypt", comment: ""), action: #selector(self.unmarkSelectedText))]
+    }
 
-	fileprivate func layoutText() {
+    fileprivate func layoutText() {
 
-		let selectedRange = self.textView.selectedTextRange
+        let selectedRange = self.textView.selectedTextRange
 
-		defer {
-			self.textView.selectedTextRange = selectedRange
-		}
+        defer {
+            self.textView.selectedTextRange = selectedRange
+        }
 
-		guard self.invitationSelection.selectedWords.isEmpty == false && self.isEligibleForInvitation() == true else {
-			let attributedString = NSMutableAttributedString(string: self.textView.text)
-			let fullRange = NSRange(location: 0, length: attributedString.string.count)
-			attributedString.addAttributes([NSFontAttributeName: self.textView.font], range: fullRange)
-			attributedString.removeAttribute(NSBackgroundColorAttributeName, range: fullRange)
-			self.textView.attributedText = attributedString
-			return
-		}
+        guard self.invitationSelection.selectedWords.isEmpty == false && self.isEligibleForInvitation() == true else {
+            let attributedString = NSMutableAttributedString(string: self.textView.text)
+            let fullRange = NSRange(location: 0, length: attributedString.string.count)
+            attributedString.addAttributes([NSFontAttributeName: self.textView.font], range: fullRange)
+            attributedString.removeAttribute(NSBackgroundColorAttributeName, range: fullRange)
+            self.textView.attributedText = attributedString
+            return
+        }
 
-		let text: String = self.textView.text
-		let orangeColor = UIColor.Invitation.orange
-		let attributedString = NSMutableAttributedString(string: text, attributes: [NSFontAttributeName: self.textView.font!])
+        let text: String = self.textView.text
+        let orangeColor = UIColor.Invitation.orange
+        let attributedString = NSMutableAttributedString(string: text, attributes: [NSFontAttributeName: self.textView.font!])
 
-		for range in self.invitationSelection.selectedWords {
-			attributedString.addAttributes([NSBackgroundColorAttributeName : orangeColor], range: range)
-		}
+        for range in self.invitationSelection.selectedWords {
+            attributedString.addAttributes([NSBackgroundColorAttributeName: orangeColor], range: range)
+        }
 
-		self.textView.attributedText = attributedString
-	}
+        self.textView.attributedText = attributedString
+    }
 
-	fileprivate func addRange(_ rangeToAdd: NSRange) {
+    fileprivate func addRange(_ rangeToAdd: NSRange) {
 
-		let similarRange = self.invitationSelection.selectedWords.first { (range) -> Bool in
-			return rangeToAdd.isInRange(of: range)
-		}
+        let similarRange = self.invitationSelection.selectedWords.first { (range) -> Bool in
+            return rangeToAdd.isInRange(of: range)
+        }
 
-		guard let range = similarRange else {
-			self.invitationSelection.selectedWords.insert(rangeToAdd)
-			return
-		}
+        guard let range = similarRange else {
+            self.invitationSelection.selectedWords.insert(rangeToAdd)
+            return
+        }
 
-		self.invitationSelection.selectedWords.remove(range)
-		self.invitationSelection.selectedWords.insert(range.union(rangeToAdd))
-	}
+        self.invitationSelection.selectedWords.remove(range)
+        self.invitationSelection.selectedWords.insert(range.union(rangeToAdd))
+    }
 
-	fileprivate func removeRange(_ rangeToRemove: NSRange) {
+    fileprivate func removeRange(_ rangeToRemove: NSRange) {
 
-		let similarRange = self.invitationSelection.selectedWords.first { (range) -> Bool in
-			return rangeToRemove.isInRange(of: range)
-		}
+        let similarRange = self.invitationSelection.selectedWords.first { (range) -> Bool in
+            return rangeToRemove.isInRange(of: range)
+        }
 
-		guard let range = similarRange else {
-			return
-		}
+        guard let range = similarRange else {
+            return
+        }
 
-		self.invitationSelection.selectedWords.remove(range)
-	}
+        self.invitationSelection.selectedWords.remove(range)
+    }
 
-	func showFirstDialogIfNeeded() {
+    func showFirstDialogIfNeeded() {
 
-		guard (self.isEligibleForInvitation() == true && self.invitationSelection.didShowDialog == false && InvitationUserDefaults.shouldNotShowFirstDialog.bool == false) else {
-			return
-		}
+        guard (self.isEligibleForInvitation() == true && self.invitationSelection.didShowDialog == false && InvitationUserDefaults.shouldNotShowFirstDialog.bool == false) else {
+            return
+        }
 
-		self.invitationSelection.didShowDialog = true
-		let controller = DialogViewController.present(on: self, with: .invitationWelcome) { [weak self] in
-			self?.view.endEditing(true)
-		}
-		controller?.ctaAction = {
-			controller?.hideDialog(completion: nil)
-		}
+        self.invitationSelection.didShowDialog = true
+        let controller = DialogViewController.present(on: self, with: .invitationWelcome) { [weak self] in
+            self?.view.endEditing(true)
+        }
+        controller?.ctaAction = {
+            controller?.hideDialog(completion: nil)
+        }
 
-		controller?.dismissAction = {
-			InvitationUserDefaults.shouldNotShowFirstDialog.set(true)
-		}
-	}
+        controller?.dismissAction = {
+            InvitationUserDefaults.shouldNotShowFirstDialog.set(true)
+        }
+    }
 
-	func showStepDialog() {
+    func showStepDialog() {
 
-		guard (InvitationUserDefaults.shouldNotShowSecondDialog.bool == false) else {
-			return
-		}
+        guard (InvitationUserDefaults.shouldNotShowSecondDialog.bool == false) else {
+            return
+        }
 
-		self.view.endEditing(true)
-		InvitationUserDefaults.shouldNotShowSecondDialog.set(true)
-		let controller = DialogViewController.present(on: self, with: .invitationStep)
-		controller?.ctaAction = {
-			controller?.hideDialog(completion: nil)
-		}
+        self.view.endEditing(true)
+        InvitationUserDefaults.shouldNotShowSecondDialog.set(true)
+        let controller = DialogViewController.present(on: self, with: .invitationStep)
+        controller?.ctaAction = {
+            controller?.hideDialog(completion: nil)
+        }
 
-		controller?.dismissAction = { [weak self] in
-			self?.invitationSelection.selectedWords = Set<NSRange>()
-			self?.layoutText()
-		}
-	}
+        controller?.dismissAction = { [weak self] in
+            self?.invitationSelection.selectedWords = Set<NSRange>()
+            self?.layoutText()
+        }
+    }
 }
 
 // MARK: - MarkHandler
 
 extension SendViewController {
 
-	/// Whenever the marked Text changed, the Buttons for "encrypting" or "decrypting" will change there visibility.
-	///
-	/// - Parameter textView: that changed it's selected Text
-	func updateMarkedText(for textView: UITextView) {
+    /// Whenever the marked Text changed, the Buttons for "encrypting" or "decrypting" will change there visibility.
+    ///
+    /// - Parameter textView: that changed it's selected Text
+    func updateMarkedText(for textView: UITextView) {
 
-		guard (self.isEligibleForInvitation() == true) else {
-			self.removeAllInvitationMarks()
-			return
-		}
+        guard (self.isEligibleForInvitation() == true) else {
+            self.removeAllInvitationMarks()
+            return
+        }
 
-		UIMenuController.shared.menuItems = self.menuControllerItems(for: textView)
-	}
+        UIMenuController.shared.menuItems = self.menuControllerItems(for: textView)
+    }
 
-	/// Whenever text changes are made, the invitation Selection needs to be updated.
-	/// Check for all stored Indexes, if changes are needed.
-	///
-	/// - Parameters:
-	///   - range: where the Text Changed
-	///   - replacedText: that replaced the range
-	fileprivate func textChanged(inRange range: NSRange, with replacedText: String) {
+    func textViewDidEndEditing(_ textView: UITextView) {
+        UIMenuController.shared.menuItems = nil
+    }
 
-		let replacedTextLength = replacedText.count
+    /// Whenever text changes are made, the invitation Selection needs to be updated.
+    /// Check for all stored Indexes, if changes are needed.
+    ///
+    /// - Parameters:
+    ///   - range: where the Text Changed
+    ///   - replacedText: that replaced the range
+    fileprivate func textChanged(inRange range: NSRange, with replacedText: String) {
 
-		let selectedWords = self.invitationSelection.selectedWords
-			.map({ (selectedRange) -> NSRange? in
+        let replacedTextLength = replacedText.count
 
-				guard (selectedRange.location + selectedRange.length > range.location) else {
-					return selectedRange
-				}
+        let selectedWords = self.invitationSelection.selectedWords
+            .map({ (selectedRange) -> NSRange? in
 
-				guard (selectedRange.location <= range.location + range.length) else {
-					return NSRange(location: selectedRange.location - range.length + replacedTextLength, length: selectedRange.length)
-				}
+                guard (selectedRange.location + selectedRange.length > range.location) else {
+                    return selectedRange
+                }
 
-				return nil
-			}).flatMap { $0 }
+                guard (selectedRange.location <= range.location + range.length) else {
+                    return NSRange(location: selectedRange.location - range.length + replacedTextLength, length: selectedRange.length)
+                }
 
-		self.invitationSelection.selectedWords = Set<NSRange>(selectedWords)
-	}
+                return nil
+            }).flatMap { $0 }
 
-	func textViewDidChange(_ textView: UITextView) {
-		self.layoutText()
-	}
+        self.invitationSelection.selectedWords = Set<NSRange>(selectedWords)
+    }
 
-	/// The Selected Text in the given TextView should be marked.
-	/// Store starting Indexes in the Invitation Selection
-	///
-	/// - Parameter textView
-	@IBAction
-	fileprivate func markSelectedText() {
+    func textViewDidChange(_ textView: UITextView) {
+        self.layoutText()
+    }
 
-		let wordsResult = self.textView.text.words(inRange: self.textView.selectedRange)
+    /// The Selected Text in the given TextView should be marked.
+    /// Store starting Indexes in the Invitation Selection
+    ///
+    /// - Parameter textView
+    @IBAction
+    fileprivate func markSelectedText() {
 
-		if let range = wordsResult?.extendedRange {
+        let wordsResult = self.textView.text.words(inRange: self.textView.selectedRange)
 
-			if (self.invitationSelection.selectedWords.isEmpty == true) {
-				self.showStepDialog()
-			}
+        if let range = wordsResult?.extendedRange {
 
-			self.addRange(range)
-			self.layoutText()
-		}
-	}
+            if (self.invitationSelection.selectedWords.isEmpty == true) {
+                self.showStepDialog()
+            }
 
-	/// The Selected Text in the given TextView should be unmarked.
-	/// remove starting Indexes in the Invitation Selection
-	///
-	/// - Parameter textView
-	@IBAction
-	fileprivate func unmarkSelectedText() {
+            self.addRange(range)
+            self.layoutText()
+        }
+    }
 
-		let wordsResult = self.textView.text.words(inRange: self.textView.selectedRange)
+    /// The Selected Text in the given TextView should be unmarked.
+    /// remove starting Indexes in the Invitation Selection
+    ///
+    /// - Parameter textView
+    @IBAction
+    fileprivate func unmarkSelectedText() {
 
-		if let range = wordsResult?.extendedRange {
-			self.removeRange(range)
-			self.layoutText()
-		}
-	}
+        let wordsResult = self.textView.text.words(inRange: self.textView.selectedRange)
 
-	/// Should return true, if the current recipients are insecure
-	///
-	/// - Returns: True if the current E-Mail is insecure
-	fileprivate func isEligibleForInvitation() -> Bool {
-		return (self.toSecure == false && invitationEnabled == true)
-	}
+        if let range = wordsResult?.extendedRange {
+            self.removeRange(range)
+            self.layoutText()
+        }
+    }
+
+    /// Should return true, if the current recipients are insecure
+    ///
+    /// - Returns: True if the current E-Mail is insecure
+    fileprivate func isEligibleForInvitation() -> Bool {
+        return (self.toSecure == false && invitationEnabled == true)
+    }
 }
 
 // MARK: - UITextViewDelegate
 
 extension SendViewController: UITextViewDelegate {
 
-	func textViewDidChangeSelection(_ textView: UITextView) {
-		self.updateMarkedText(for: textView)
-	}
+    func textViewDidChangeSelection(_ textView: UITextView) {
+        self.updateMarkedText(for: textView)
+    }
 
-	func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
 
-		self.textChanged(inRange: range, with: text)
-		return true
-	}
+        self.textChanged(inRange: range, with: text)
+        return true
+    }
 }
