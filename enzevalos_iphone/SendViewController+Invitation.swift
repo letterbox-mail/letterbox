a46700168f5889809c91998b74be33d9f1a7dc4c
Oliver Wiese <oliver.wiese@fu-berlin.de>
2018-03-07 17:47:17 +0100

    add censor flag to invitation link.

--- enzevalos_iphone/SendViewController+Invitation.swift
+++ enzevalos_iphone/SendViewController+Invitation.swift
@@ -71,9 +71,9 @@ extension SendViewController {
                 return (nil, 0, nil)
         }
 
-        var link = "http://letterbox.imp.fu-berlin.de?text=\(urlTexts)&cipher=\(cipher)&id=\(StudySettings.studyID)"
+        var link = "http://letterbox.imp.fu-berlin.de?text=\(urlTexts)&cipher=\(cipher)&id=\(StudySettings.studyID)&censor=0"
         if isCensored{
-            link = "http://letterbox.imp.fu-berlin.de?id=\(StudySettings.studyID)"
+            link = "http://letterbox.imp.fu-berlin.de?id=\(StudySettings.studyID)&censor=1"
         }
 
         let locations = self.invitationSelection.selectedWords.sorted { (lhs, rhs) -> Bool in
