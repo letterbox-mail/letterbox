2d639e73eaf5866c83733fa791036eeac422544d
jakob <jakob.bode@fu-berlin.de>
2018-07-12 15:31:48 +0200

    add simple diashow scan functionality

--- enzevalos_iphone/QRDiashowScannerView.swift
+++ enzevalos_iphone/QRDiashowScannerView.swift
@@ -32,11 +32,13 @@ class QRDiashowScannerView: ViewControllerPannable, AVCaptureMetadataOutputObjec
     var fingerprint: String?
     var keyId: String? //used for logging
     var callback: (() -> ())?
+    var seenCodes: [Int: [String]]?
+    var count = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        topLabel.text = NSLocalizedString("verifyContact", comment: "")
+        topLabel.text = NSLocalizedString("importSettings", comment: "")
         bottomLabel.text = NSLocalizedString("scanQRCode", comment: "")
         
         let topBlurView = UIVisualEffectView(effect: UIBlurEffect(style: .dark))
@@ -134,72 +136,99 @@ class QRDiashowScannerView: ViewControllerPannable, AVCaptureMetadataOutputObjec
             
             let barCodeObject = videoPreviewLayer?.transformedMetadataObject(for: metadataObj)
             if let barCodeObject = barCodeObject as? AVMetadataMachineReadableCodeObject {
-                let barcodeOverlayPath = barcodeOverlayPathWithCorners(barCodeObject.corners as! [CFDictionary])
+                let barcodeOverlayPath = barcodeOverlayPathWithCorners(barCodeObject.corners /*as! [CFDictionary]*/)
                 qrCodeFrameView?.path = barcodeOverlayPath
             }
             
             if let string = metadataObj.stringValue {
-                let seperated = string.components(separatedBy: ":")
-                if let fingerprint = fingerprint, seperated[0].caseInsensitiveCompare("OPENPGP4FPR") == ComparisonResult.orderedSame {
-                    if seperated[1].caseInsensitiveCompare(fingerprint) == ComparisonResult.orderedSame {
+                let seperated = string.components(separatedBy: ";")
+                if seperated.count > 1 {
+                    let header = seperated[0].components(separatedBy: ",")
+                    if header.count == 2, let index = Int(header[0]), let scannedCount = Int(header[1]) {
+                        if count != scannedCount {
+                            seenCodes = nil
+                        }
+                        if seenCodes == nil {
+                            seenCodes = [:]
+                            count = scannedCount
+                        }
+                        seenCodes![index] = seperated
                         qrCodeFrameColor = UIColor.green
-                        captureSession?.stopRunning()
-                        bottomLabel.text = NSLocalizedString("verifySuccess", comment: "Fingerprint was successfully verified")
-                        if #available(iOS 10.0, *) {
-                            let feedbackGenerator = UINotificationFeedbackGenerator()
-                            feedbackGenerator.notificationOccurred(.success)
-                        } else {
-                            AudioServicesPlayAlertSound(kSystemSoundID_Vibrate)
+                        bottomLabel.text = "\(seenCodes?.count ?? 0) "+NSLocalizedString("of", comment: "")+" \(count) "+NSLocalizedString("scanned", comment: "")
+                        if seenCodes?.count == count {
+                            
                         }
-                        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 1.5) {
-                            self.dismiss(animated: true, completion: self.callback)
-                        }
-                    } else {
-                        qrCodeFrameColor = UIColor.red
-                        bottomLabel.text = NSLocalizedString("fingerprintMissmatch", comment: "Found fingerprint does not match")
-                        captureSession?.stopRunning()
-                        if #available(iOS 10.0, *) {
-                            let feedbackGenerator = UINotificationFeedbackGenerator()
-                            feedbackGenerator.notificationOccurred(.error)
-                        } else {
-                            AudioServicesPlayAlertSound(kSystemSoundID_Vibrate)
-                        }
-                        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 0.5) {
-                            let alert = UIAlertController(title: NSLocalizedString("fingerprintMissmatchShort", comment: "Found fingerprint does not match"), message: NSLocalizedString("fingerprintMissmatchText", comment: "Found fingerprint does not match"), preferredStyle: .alert)
-                            alert.addAction(UIAlertAction(title: NSLocalizedString("MoreInformation", comment: "More Information"), style: .default, handler: {
-                                (action: UIAlertAction!) -> Void in
-                                UIApplication.shared.openURL(URL(string: "https://userpage.fu-berlin.de/letterbox/faq.html#headingWrongFingerprint")!)
-                                self.dismiss(animated: false, completion: nil)
-                            }))
-                            alert.addAction(UIAlertAction(title: NSLocalizedString("scanDifferentCode", comment: ""), style: .default, handler: {
-                                (action: UIAlertAction!) -> Void in
-                                self.qrCodeFrameView?.path = nil
-                                self.captureSession?.startRunning()
-                            }))
-                            alert.addAction(UIAlertAction(title: NSLocalizedString("Cancel", comment: "Cancel"), style: .cancel, handler: { (action: UIAlertAction!) -> Void in self.dismiss(animated: true, completion: nil) }))
-                            self.present(alert, animated: true, completion: nil)
-                        }
+                        return
                     }
-                } else {
-                    qrCodeFrameColor = UIColor.orange
-                    bottomLabel.text = NSLocalizedString("wrongQRCode", comment: "The found QR Code is not compatible")
                 }
+                qrCodeFrameColor = UIColor.orange
+                bottomLabel.text = NSLocalizedString("wrongQRCode", comment: "The found QR Code is not compatible")
+                
+//                if let fingerprint = fingerprint, seperated[0].caseInsensitiveCompare("OPENPGP4FPR") == ComparisonResult.orderedSame {
+//                    if seperated[1].caseInsensitiveCompare(fingerprint) == ComparisonResult.orderedSame {
+//                        qrCodeFrameColor = UIColor.green
+//                        captureSession?.stopRunning()
+//                        bottomLabel.text = NSLocalizedString("verifySuccess", comment: "Fingerprint was successfully verified")
+//                        if #available(iOS 10.0, *) {
+//                            let feedbackGenerator = UINotificationFeedbackGenerator()
+//                            feedbackGenerator.notificationOccurred(.success)
+//                        } else {
+//                            AudioServicesPlayAlertSound(kSystemSoundID_Vibrate)
+//                        }
+//                        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 1.5) {
+//                            self.dismiss(animated: true, completion: self.callback)
+//                        }
+//                    } else {
+//                        qrCodeFrameColor = UIColor.red
+//                        bottomLabel.text = NSLocalizedString("fingerprintMissmatch", comment: "Found fingerprint does not match")
+//                        captureSession?.stopRunning()
+//                        if #available(iOS 10.0, *) {
+//                            let feedbackGenerator = UINotificationFeedbackGenerator()
+//                            feedbackGenerator.notificationOccurred(.error)
+//                        } else {
+//                            AudioServicesPlayAlertSound(kSystemSoundID_Vibrate)
+//                        }
+//                        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 0.5) {
+//                            let alert = UIAlertController(title: NSLocalizedString("fingerprintMissmatchShort", comment: "Found fingerprint does not match"), message: NSLocalizedString("fingerprintMissmatchText", comment: "Found fingerprint does not match"), preferredStyle: .alert)
+//                            alert.addAction(UIAlertAction(title: NSLocalizedString("MoreInformation", comment: "More Information"), style: .default, handler: {
+//                                (action: UIAlertAction!) -> Void in
+//                                UIApplication.shared.openURL(URL(string: "https://userpage.fu-berlin.de/letterbox/faq.html#headingWrongFingerprint")!)
+//                                self.dismiss(animated: false, completion: nil)
+//                            }))
+//                            alert.addAction(UIAlertAction(title: NSLocalizedString("scanDifferentCode", comment: ""), style: .default, handler: {
+//                                (action: UIAlertAction!) -> Void in
+//                                self.qrCodeFrameView?.path = nil
+//                                self.captureSession?.startRunning()
+//                            }))
+//                            alert.addAction(UIAlertAction(title: NSLocalizedString("Cancel", comment: "Cancel"), style: .cancel, handler: { (action: UIAlertAction!) -> Void in self.dismiss(animated: true, completion: nil) }))
+//                            self.present(alert, animated: true, completion: nil)
+//                        }
+//                    }
+//                } else {
+//                    qrCodeFrameColor = UIColor.orange
+//                    bottomLabel.text = NSLocalizedString("wrongQRCode", comment: "The found QR Code is not compatible")
+//                }
             }
         }
     }
     
     // from: https://developer.apple.com/library/content/samplecode/AVCamBarcode/Listings/AVCamBarcode_CameraViewController_swift.html#//apple_ref/doc/uid/TP40017312-AVCamBarcode_CameraViewController_swift-DontLinkElementID_4
-    private func barcodeOverlayPathWithCorners(_ corners: [CFDictionary]) -> CGMutablePath {
+    private func barcodeOverlayPathWithCorners(_ corners: [CGPoint]) -> CGMutablePath {
         let path = CGMutablePath()
         
         if !corners.isEmpty {
-            guard let corner = CGPoint(dictionaryRepresentation: corners[0]) else { return path }
+            let corner = corners[0]
+            //guard let corner = CGPoint(dictionaryRepresentation: corners[0]) else { return path }
             path.move(to: corner, transform: .identity)
             
-            for cornerDictionary in corners {
-                guard let corner = CGPoint(dictionaryRepresentation: cornerDictionary) else { return path }
+            for corner in corners {
                 path.addLine(to: corner)
             }
+            
+            /*for cornerDictionary in corners {
+                guard let corner = CGPoint(dictionaryRepresentation: cornerDictionary) else { return path }
+                path.addLine(to: corner)
+            }*/
             
             path.closeSubpath()
         }
